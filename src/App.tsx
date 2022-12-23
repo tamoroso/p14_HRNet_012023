2e4f513075a1b1b33f55bb44c47a27e8855533a9
tamoroso <amorosothomas@orange.fr>
2022-12-24 00:01:56 +0100

    Add configurations and setting up MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Typography, Stack } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Container maxWidth='md'>
+         <Stack
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
+            spacing={2}
+            height='100vh'
+         >
+            <Typography>MUI was successfully imported</Typography>
+            <Button variant='outlined'>IT&apos;S DONE</Button>
+         </Stack>
+      </Container>
+   );
 }
 
 export default App;
